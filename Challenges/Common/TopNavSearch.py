63685fc7f6580077b7ac6b8a6956cb69214a1b42
rossbenedict <ross.benedict@gmail.com>
2020-01-15 16:42:48 -0700

    Start working on challenge 5

--- Challenges/Common/TopNavSearch.py
+++ Challenges/Common/TopNavSearch.py
@@ -1,13 +1,19 @@
 #import from chalenge 2
 
-class TopNavSearch
+class TopNavSearch:
     def __init__(self, driver):
         self.driver = driver
 
     def runSearch(self, query):
-        #how to stay in the existing browser.
-        #add search from challenge 2
+        self.driver.get("https://www.copart.com")
 
+        self.searchfield = self.driver.find_element(By.ID, "input-search")
+        self.searchfield.click()
+        self.searchfield.send_keys(query)
+        self.searchbutton = self.driver.find_element(By.XPATH, '//*[@id="search-form"]/div/div[2]/button')
+        self.searchbutton.click()
+
+#how to stay in the existing browser. line 8
 #excersize the drop down on copart with number of results
 #grab all of the data from the model column
 #validate the set of data
@@ -17,4 +23,4 @@ class TopNavSearch
 
 #put values into a dictionary
 
-    def
+    #def
